389d2a52f9f012caeafeff540bedfd0c9eedf2ba
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-01-30 14:50:35 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script setup>
 import swapiVue from './components/swapi.vue';
 
+import ApiURL from './store/apiStorage';
+
+
+console.log(ApiURL)
 </script>
 
 <template>
 
   <header>
     <h2>SWAPI</h2>
+
+    <nav>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+
   </header>
 
   <swapiVue />
 </template>
+
+
+<style>
+
+header {
+  background-color: aqua;
+}
+
+</style>
+
+

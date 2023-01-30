f1da7a88b633c16e78fbfc1f5aa26e8c2696f4a5
MadeleneFasth <113009672+MadeleneFasth@users.noreply.github.com>
2023-01-30 14:07:17 +0100

    remove vue CSS

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import swapiVue from './components/swapi.vue';
+
+</script>
+
+<template>
+
+  <header>
+    <h2>SWAPI</h2>
+  </header>
+
+  <swapiVue />
+</template>
---
+++ src/components/swapi.vue
@@ -0,0 +1,21 @@
+<!-- Det som är bortkommenterat är kvar som mall/exempel -->
+
+<!-- <script>
+// import TodoItem from './TodoItem.vue';
+
+
+export default {
+  
+}
+
+</script> -->
+
+
+<template>
+    <h3>Hej</h3>
+</template>
+
+<!-- 
+<style>
+
+</style> -->
